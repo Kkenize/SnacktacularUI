bd71ab1c3045dfe5524d74136025bed8e4512e78
YourName <Kkenizez@gmail.com>
2025-03-30 18:27:01 -0400

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Zhejun Zhang on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
