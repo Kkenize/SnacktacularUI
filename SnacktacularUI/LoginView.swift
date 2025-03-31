2c9e3dc48b98807724e32b65e50d35ae7ab96c4f
YourName <Kkenizez@gmail.com>
2025-03-30 23:41:32 -0400

    New App with Firebase!

---
+++ SnacktacularUI/LoginView.swift
@@ -0,0 +1,117 @@
+//
+//  ContentView.swift
+//  SnacktacularUI
+//
+//  Created by Zhejun Zhang on 3/30/25.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+
+struct LoginView: View {
+    enum Field {
+        case email, password
+    }
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    @State private var buttonDisabled = true
+    @FocusState private var focusField: Field?
+    
+    var body: some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+            
+            Group {
+                TextField("email", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .email)
+                    .onSubmit {
+                        focusField = .password
+                    }
+                    .onChange(of: email) {
+                        enableButtons()
+                    }
+                
+                SecureField("password", text: $password)
+                    .submitLabel(.done)
+                    .focused($focusField, equals: .password)
+                    .onSubmit {
+                        focusField = nil
+                    }
+            }
+            .textFieldStyle(.roundedBorder)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(.gray.opacity(0.5), lineWidth: 2)
+            }
+            .onChange(of: email) {
+                enableButtons()
+            }
+            
+            HStack {
+                Button("Sign up") {
+                    register()
+                }
+                .padding(.trailing)
+                
+                Button("Log In") {
+                    login()
+                }
+                .padding(.leading)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.snack)
+            .font(.title2)
+            .padding(.top)
+            .disabled(buttonDisabled)
+        }
+        .padding()
+        .alert(alertMessage, isPresented: $showingAlert) {
+            Button("OK", role: .cancel) { }
+        }
+    }
+    
+    func enableButtons() {
+        let emailIsGood = email.count >= 6 && email.contains("@")
+        let passwordIsGood = password.count >= 6
+        buttonDisabled = !(emailIsGood && passwordIsGood)
+    }
+    
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("😡 Login Error: \(error.localizedDescription)")
+                alertMessage = "😡 Login Error: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("😎 Registration Success!")
+            }
+        }
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) {
+            result, error in
+            if let error = error {
+                print("😡 Login Error: \(error.localizedDescription)")
+                alertMessage = "😡 Login Error: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("🪵 Login Success!")
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
